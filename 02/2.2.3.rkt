4e3a56df16fcd8b1b3aefce3e1c6b3f2a854dd83
kk2170 <chobi.kimura@gmail.com>
2013-05-27 19:27:01 +0900

    2.36解けてないです

--- 02/2.2.3.rkt
+++ 02/2.2.3.rkt
@@ -13,8 +13,36 @@
   (accumulate cons seq1 seq2))
 
 (define (my-length sequence)
-  (accumulate  0 sequence))
+  (accumulate length 0 sequence))
 
 ;(my-map * (list 1 2 3))
-(my-append (list 1 2 3) (list 1 2 3))
-(my-length (list 1 2 3))
+;(my-append (list 1 2 3) (list 1 2 3))
+;(my-length (list 1 2 3))
+
+;Q2.34
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higer-terms)
+               (+ this-coeff (* x higer-terms)))
+                               
+             0
+             coefficient-sequence))
+(horner-eval 2 (list 1 3 0 5 0 1))
+;Q2.35
+(define (count-leaves t)
+  (accumulate +  0 (map 
+                    (lambda (x)(cond ((null? x)0)
+                                     ((not (pair? x))1)
+                                     (else(count-leaves x))
+                                   
+                      ))t)))
+
+  (count-leaves (list (list 1 2 ) (list 3 4 )))
+
+;Q2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init(car seqs))
+           (accumulate-n op init (cdr seqs)))))
+(define s (list (list 1 2 3) (list 4 5 6 ) (list 7 8 9 ) (list 10 11 12)))
+;(accumulate-n + 0 s)
