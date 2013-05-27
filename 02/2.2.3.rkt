05d1be6b40f30aa726f0887bcfa6c5f9c64b550e
kk2170 <chobi.kimura@gmail.com>
2013-05-27 19:38:20 +0900

    2.36解けてないです

--- 02/2.2.3.rkt
+++ 02/2.2.3.rkt
@@ -42,7 +42,7 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
-      (cons (accumulate op init(car seqs))
-           (accumulate-n op init (cdr seqs)))))
+      (cons (accumulate op init(map +(car seqs)))
+           (accumulate-n op init (map +(cdr seqs))))))
 (define s (list (list 1 2 3) (list 4 5 6 ) (list 7 8 9 ) (list 10 11 12)))
-;(accumulate-n + 0 s)
+(accumulate-n + 0 s)
