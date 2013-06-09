71e60f0e669235718f510d410cc02e085fc1f6ba
kk2170 <chobi.kimura@gmail.com>
2013-06-10 01:11:40 +0900

    2.21~29まで解いた

--- sicp.rkt
+++ sicp.rkt
@@ -1,9 +1,75 @@
+
+;Q2.21
+(define (square-list1 items)
+  (if (null? items)
+      '()
+      (cons (*(car items)(car items)) (square-list1 (cdr items)))))
+
+(define (square-list2 items)
+  (map (lambda (x) (* x x )) items))
+(square-list1 (list 1 2 3 4 ))
+(square-list2 (list 1 2 3 4))
+
+;Q2.22
+(define (square x)
+  (* x x ))
+(define (square-list3 items)
+  (define (iter things answer)
+    (if (null? things)
+	answer
+	(iter (cdr things)
+	      (cons (square (car things))
+		    answer))))
+  (iter items '()))
+(square-list3 (list 1 2 3 4 5))
+;こちらはcdrが先にする来るような形で実行を行なっているため
+(define (square-list4 items)
+  (define (iter things answer)
+    (if (null? things)
+	answer
+	(iter (cdr things)
+	      (cons answer
+		    (square (car things))))))
+  (iter items '()))
+(square-list4 (list 1 2 3 4 5))
+;こちらは何も考えず逆につないだ結果、consの性質で発生した。
+;Q.23
+(define (my-for-each x y)
+  (cond (null? y)
+      (x (car y))
+      (else(x (car y))(my-for-each x (cdr y)))))
+(my-for-each (lambda (x ) (newline) (display x))(list  57 321 88))
+
+;Q2.24
+
+;Q2.25
+(car(cdr(car(cdr (cdr (list 1 3 (list 5 7 ) 9))))))
+(car(car(list(list  7)) ))
+(car(cdr(car(cdr(car(cdr(car(cdr(car(cdr(car(cdr(list 1 (list 2 (list 3 (list 4(list 5(list 6 7)))))) ))))))))))))
+
+;Q2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(append x y)
+(cons x y)
+(list x y)
+
+;Q2.27
+(define (deep-reverse x)
+  (cond
+    ((null? x) x)
+    ((list? (car x))(append (deep-reverse(cdr x))(deep-reverse(car x))))
+    (else  (list(reverse x)))
+    )
+  )
+(deep-reverse (list (list 1 2 ) (list 3 4)))
 ;accumulate
 (define (accumulate op initial sequence)
   (if (null? sequence )
       initial
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
+
 ;enumerate-interval
 (define (enumerate-interval low high)
   (if (> low high)
